94f321bae53be844a5b9dbcbd4ea8bc8a6e99f4c
AngelDB06 <adombar413@g.educaand.es>
2025-01-14 19:41:23 +0100

    update

--- Bicicleta.java
+++ Bicicleta.java
@@ -1,6 +1,27 @@
 public class Bicicleta {
-    public static final String color = "Negro";
-    public static final int velocidad= 0;
-    
+    String color="Negro";
+    int velocidad=0, marcha=2;
 
+    void Avanzar(int v){
+        velocidad=v;
+        System.out.println("La velocidad actual es de "+velocidad+" KM/H.");
+    }
+
+    void Cambiomarcha(int m){
+        marcha=m;
+        System.out.println("La marcha actual es "+marcha+".");
+    }
+
+    void Frenar(){
+        System.out.println("La bicicleta ha frenado.");
+    }
+
+    public static void main(String[] args) {
+        Bicicleta b1=new Bicicleta();
+
+        b1.Avanzar(20);
+        b1.Cambiomarcha(5);
+        b1.Frenar();
+        System.out.println("El color de la bicicleta es "+b1.color+".");
+    }
 }
