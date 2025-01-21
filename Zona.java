080be91c01b43e1b033e0a1e7a82f86ec39d79e5
AngelDB06 <adombar413@g.educaand.es>
2025-01-21 18:29:29 +0100

    update

---
+++ Zona.java
@@ -0,0 +1,12 @@
+public class Zona {
+    private int entradasPorVender;
+
+    public Zona(int n){
+        entradasPorVender = n;
+    }
+
+    public int getEntradasPorVender() {
+        return entradasPorVender;
+    }
+    
+}
