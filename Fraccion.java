b3cc8e237535d0ae356fd42b4cce15899d740bbb
AngelDB06 <adombar413@g.educaand.es>
2025-01-21 10:04:31 +0100

    update

--- Fraccion.java
+++ Fraccion.java
@@ -38,13 +38,33 @@ public class Fraccion {
 
         return (f3);
     }
+
+    static Fraccion Simplificar(Fraccion f1){
+        int divisor=Math.min(f1.getDenominador(), f1.getNumerador()),numerador=Math.max(f1.getDenominador(), f1.getNumerador());
+        int divisorog,numeradorog;
+        divisorog=divisor;
+        numeradorog=numerador;
+        Fraccion f4=new Fraccion();
+
+       while (numerador%divisor!=0) {
+            if (numerador%divisor!=0) {
+                int temp=numerador;
+                numerador=divisor;
+                divisor=temp%divisor;
+            }
+        }
+        f4.setFraccion(numeradorog/divisor, divisorog/divisor);
+        return f4;
+    }
     public static void main(String[] args) {
         Fraccion f1=new Fraccion();
         Fraccion f2=new Fraccion();
         Fraccion f3=new Fraccion();
 
-        f1.setFraccion(2, 4);
+        
+        f1.setFraccion(10, 5);
         f2.setFraccion(3, 6);
+        
         f3= Fraccion.Multiplicar(f1, f2);
         
         System.out.println("El resultado de la multiplicación es "+f3.getNumerador()+"/"+f3.getDenominador());
@@ -53,6 +73,9 @@ public class Fraccion {
 
         System.out.println("El resultado de la division es "+f3.getNumerador()+"/"+f3.getDenominador());
 
+        f3= Fraccion.Simplificar(f1);
+        
+        System.out.println("Numerador: "+f3.getNumerador()+", Denominador: "+f3.getDenominador());
         
     }
 }
